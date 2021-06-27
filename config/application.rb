7074fb5eb2d0ea54bdcd64b02fc1f610b87810ed
Tomoya Ikegami <60150746+tom0418@users.noreply.github.com>
2021-06-28 08:45:49 +0900

    Rails Appに最低限の設定を追加

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@
 require_relative 'boot'
 
 require 'rails'
-# Pick the frameworks you want:
 require 'active_model/railtie'
 require 'active_job/railtie'
 require 'active_record/railtie'
@@ -14,27 +13,22 @@ require 'action_mailbox/engine'
 require 'action_text/engine'
 require 'action_view/railtie'
 require 'action_cable/engine'
-# require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module TasksManagerServer
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.add_autoload_paths_to_load_path = false
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Set Timezone
+    config.time_zone = 'Tokyo'
+
+    # Set i18n default locale
+    # rails-i18nを入れたらコメントイン
+    # config.i18n.default_locale = :ja
 
     # Generate test framework
     config.generators do |g|
