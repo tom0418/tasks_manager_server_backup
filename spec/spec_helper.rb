952d5dcecdf81b089192fdd9a9aa44f8675ac378
Tomoya Ikegami <60150746+tom0418@users.noreply.github.com>
2021-06-29 16:15:14 +0900

    shoulda-matchersの設定

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,3 +14,12 @@ RSpec.configure do |config|
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :active_record
+    with.library :active_model
+    with.library :action_controller
+  end
+end
